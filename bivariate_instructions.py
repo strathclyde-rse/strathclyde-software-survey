d9992705fde4ba7e4204110061c68c2755d574be
Oliver Henrich <ohenrich@users.noreply.github.com>
2020-09-29 22:02:42 +0100

    New interim analysis and report

--- bivariate_instructions.py
+++ bivariate_instructions.py
@@ -3,9 +3,9 @@
 
 which_by_which = {
 
-    'faculty': ['version_control', 'unit_regression_testing', 'continuous_integration']
-
+#    'faculty': ['version_control', 'unit_regression_testing', 'continuous_integration']
 #    'faculty': ['use_software', 'training', 'develop_own_code', 'development_expertise', 'training', 'want_to_commercialise', 'version_control', 'funds_for_development'],
 #    'job_title': ['develop_own_code'],
-#    'importance_software': ['funds_for_development']
+    'funders': ['costed_developer']
+
 }
